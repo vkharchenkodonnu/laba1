7b86da825387e41e20b66d3dbf31ac72c97dbd03
Владислав Харченко <kharchenko.v@donnu.edu.ua>
2020-09-16 11:44:45 +0300

    class

--- test_pr/test_pr/Program.cs
+++ test_pr/test_pr/Program.cs
@@ -6,8 +6,16 @@ namespace test_pr
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Nyuhay Bebru");
+            string name = "Tom";
+            int age = 33;
+            bool isEmployed = false;
+            double weight = 78.65;
+            Console.WriteLine($"Имя: {name}");
+            Console.WriteLine($"Возраст:{ age}");
+            Console.WriteLine($"Вес: {weight}");
+            Console.WriteLine($"Работает: {isEmployed} ");
+            Console.WriteLine($"Имя: {name}  Возраст {age}  Вес {weight}");
+            Console.WriteLine("Имя: {0}  Возраст: {2}  Вес: {1}kg", name, weight, age);
         }
     }
 }
